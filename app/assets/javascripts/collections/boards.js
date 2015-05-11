855c48aff2c5da9c10d6fbd18bb3f186c392e72b
Nathan Hinchey <nathanhinchey@gmail.com>
2015-05-11 16:30:53 -0400

    Move getOrFetch from Boards into Backbone.Collection.prototype

--- app/assets/javascripts/collections/boards.js
+++ app/assets/javascripts/collections/boards.js
@@ -1,22 +1,6 @@
 TrelloClone.Collections.Boards = Backbone.Collection.extend({
   model: TrelloClone.Models.Board,
 
-  url: "api/boards",
+  url: "api/boards"
 
-  getOrFetch: function (id) {
-    var model = this.get(id);
-    var collection = this;
-    if (!model) {
-      model = new this.model({id: id})
-      model.fetch({
-        success: function () {
-          collection.add(model);
-        }
-      });
-    } else {
-      model.fetch();
-    }
-
-    return model;
-  }
 })
--- app/assets/javascripts/utils/getOrFetch.js
+++ app/assets/javascripts/utils/getOrFetch.js
@@ -1,20 +1,22 @@
-// Backbone.Collection.getOrFetch = function (id) {
-//   var model = this.get(id);
-//   var collection = this;
-//   if (!model) {
-//     model = new this.model({id: id})
-//     model.fetch({
-//       success: function () {
-//         collection.add(model);
-//       }
-//     });
-//   } else {
-//     model.fetch();
-//   }
-//
-//   return model;
-// }
+Backbone.Collection.prototype.getOrFetch = function (id) {
+  var model = this.get(id);
+  var collection = this;
+  if (!model) {
+    model = new this.model({id: id})
+    model.fetch({
+      success: function () {
+        collection.add(model);
+      }
+    });
+  } else {
+    model.fetch();
+  }
 
-// _QUESTION_
+  return model;
+}
+
+// QUESTION:
 // Why doesn't this add an instance method to the collections that use
 // Backbone.Collection.extend?
+// _ANSWER_
+// Use .prototype -- that's how you make 'instance' methods in JS
