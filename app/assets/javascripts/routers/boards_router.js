dd05b21f4359ed76de76431aa84ef6c5d1741355
Nathan Hinchey <nathanhinchey@gmail.com>
2015-05-11 11:21:03 -0400

    Create functioning backbone 'new' form

--- app/assets/javascripts/routers/boards_router.js
+++ app/assets/javascripts/routers/boards_router.js
@@ -13,6 +13,19 @@ TrelloClone.Routers.BoardsRouter = Backbone.Router.extend({
     var board = new TrelloClone.Models.Board({id: id});
     board.fetch();
     var view = new TrelloClone.Views.BoardShow({model: board});
-    this.$contentEl.html(view.render().$el);
+    this._swapViews(view);
+  },
+
+  new: function () {
+    var view = new TrelloClone.Views.BoardNew();
+    this._swapViews(view);
+  },
+
+  _swapViews: function (view) {
+    if (this._view){
+      this._view.remove();
+    }
+    this._view = view;
+    this.$contentEl.html(view.render().$el)
   }
 });
---
+++ app/assets/javascripts/views/boards/new.js
@@ -0,0 +1,31 @@
+TrelloClone.Views.BoardNew = Backbone.View.extend({
+  template: JST["boards/new"],
+
+  tagName: 'form',
+
+  render: function () {
+    var content = this.template();
+    this.$el.html(content);
+
+    return this;
+  },
+
+  events: {
+    'submit': 'submit'
+  },
+
+  submit: function (event) {
+    event.preventDefault();
+    var params = this.$el.serializeJSON();
+    var board = new TrelloClone.Models.Board(params['board']);
+    console.log(this.$el);
+
+    board.save({}, {
+      success: function () {
+        Backbone.history.navigate("boards/" + board.id, {
+          trigger: true
+        })
+      }
+    });
+  }
+})
